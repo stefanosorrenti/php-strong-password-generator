83c8fa4694bd582ef08958b752f92b00601cad3d
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-06-11 12:03:45 +0200

    MILESTONE 1: add HTML form base

---
+++ server.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generatore password</title>
+</head>
+
+<!-- Header -->
+<header>
+    <h1>Generatore password</h1>
+</header>
+
+<!-- Body -->
+<body>
+    <!-- FORM -->
+    <div>   
+
+        <form>
+            <label for="length">Lunghezza della password</label>
+            <input type="number" name="length" id="length">
+            <button>Genera</button>
+        </form>
+
+    </div>
+
+</body>
+
+</html>
